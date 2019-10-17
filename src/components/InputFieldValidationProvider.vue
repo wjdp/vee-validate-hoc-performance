97ed8aed3e867d3c357e5da813a980e84da9f19a
Will Pimblett <will@wjdp.uk>
2019-10-17 14:46:58 +0100

    Try with ValidationProvider

---
+++ src/components/InputFieldValidationProvider.vue
@@ -0,0 +1,20 @@
+<template>
+    <ValidationProvider :rules="rules" v-slot="{errors, invalid, required}">
+        <InputField :value="value"
+                    @input="$emit('input', $event)"
+                    :errors="errors"
+                    :invalid="invalid"
+                    :required="required"/>
+    </ValidationProvider>
+</template>
+
+<script>
+    import {ValidationProvider} from "vee-validate"
+    import InputField from "./InputField";
+
+    export default {
+        name: "InputFieldValidationProvider",
+        props: ["rules", "value"],
+        components: {ValidationProvider, InputField}
+    };
+</script>
